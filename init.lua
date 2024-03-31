2f04ec36ebf7f9641e9bffb913dbe0f5c0ba517b
ohzw <52651644+ohzw@users.noreply.github.com>
2024-03-31 20:14:18 +0900

    configure vue format

--- init.lua
+++ init.lua
@@ -633,6 +633,7 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        vue = { 'prettierd', 'eslint' },
       },
     },
   },
