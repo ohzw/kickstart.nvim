c9068bdb2eb37f6ef4eb0412930a6740f5af555c
ohzw <52651644+ohzw@users.noreply.github.com>
2024-03-31 19:39:09 +0900

    add neoscroll

--- init.lua
+++ init.lua
@@ -842,6 +842,7 @@ require('lazy').setup({
   require 'kickstart.plugins.filetree',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.multi_cursor',
+  require 'kickstart.plugins.scroll',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
