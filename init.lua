304c923f393a21339dba156ca63dd32117c45798
ohzw <52651644+ohzw@users.noreply.github.com>
2024-04-03 19:30:30 +0900

    add tsserver

--- init.lua
+++ init.lua
@@ -597,6 +597,7 @@ require('lazy').setup({
         -- tsserver = {},
         --
 
+        tsserver = {},
         volar = {
           filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
           init_options = {
