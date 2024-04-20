4ed175adb8281fab62d0d84c54ed2f0014259d92
ohzw <52651644+ohzw@users.noreply.github.com>
2024-04-20 20:10:30 +0900

    add mini.starter

--- init.lua
+++ init.lua
@@ -835,22 +835,68 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- -- Simple and easy statusline.
-      -- --  You could remove this setup call if you don't like it,
-      -- --  and try some other statusline plugin
-      -- local statusline = require 'mini.statusline'
-      -- -- set use_icons to true if you have a Nerd Font
-      -- statusline.setup { use_icons = vim.g.have_nerd_font }
-      --
-      -- -- You can configure sections in the statusline by overriding their
-      -- -- default behavior. For example, here we set the section for
-      -- -- cursor location to LINE:COLUMN
-      -- ---@diagnostic disable-next-line: duplicate-set-field
-      -- statusline.section_location = function()
-      --   return '%2l:%-2v'
-      -- end
+      require('mini.starter').setup {
+        -- Whether to open starter buffer on VimEnter. Not opened if Neovim was
+        -- started with intent to show something else.
+        autoopen = true,
 
-      -- ... and there is more!
+        -- Whether to evaluate action of single active item
+        evaluate_single = false,
+
+        -- Items to be displayed. Should be an array with the following elements:
+        -- - Item: table with <action>, <name>, and <section> keys.
+        -- - Function: should return one of these three categories.
+        -- - Array: elements of these three types (i.e. item, array, function).
+        -- If `nil` (default), default items will be used (see |mini.starter|).
+        items = nil,
+
+        -- Header to be displayed before items. Converted to single string via
+        -- `tostring` (use `\n` to display several lines). If function, it is
+        -- evaluated first. If `nil` (default), polite greeting will be used.
+        header = [[
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠞⢓⡒⢭⣇⣤⠤⡄⡀⠤⠄⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡊⠐⠁⢀⡴⠅⠂⠉⠉⠈⠉⠐⠂⠪⠤⢉⠙⠖⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡐⠝⠁⠀⠊⠀⠀⠀⡀⢄⡂⠠⠀⠀⠀⠀⠀⠀⠈⠱⢜⠣⣄⠀⣀⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠊⢠⡆⠀⠀⠀⢠⠊⠊⠀⠀⠀⠀⠀⠀⠐⠈⠐⡄⠀⠀⢩⠐⡇⠉⠐⡍⢲⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⢀⣀⣀⡰⠇⣰⡫⠀⠀⠀⡼⠃⠀⠀⠀⠀⡠⢃⠞⣠⠀⠀⠁⡘⡄⠀⠈⢆⢰⡌⠐⠌⠄⢱⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⣌⠹⣷⣾⢩⢍⣘⠳⠁⠀⠀⠌⠀⠀⣀⠠⣐⠨⠀⠁⠸⢀⠀⠀⠀⠐⡰⠀⠀⠈⡜⣙⡄⠈⠴⢺⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⣢⡉⠸⡀⣮⡇⠀⠰⡘⠈⠉⠙⠍⣁⠜⠀⠀⠀⠀⣩⡄⠀⠀⠀⢡⠃⠀⠀⢠⡿⢹⡀⠒⡼⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠗⣮⡳⣜⡇⠀⠢⢁⣠⣀⡀⠀⠂⠀⠀⠀⠀⠘⢄⠻⢆⠀⠀⢸⠀⠀⠀⠈⣿⢸⢃⠸⣏⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⣚⣼⣘⣦⣴⢀⣿⡀⠀⠈⠉⠛⠋⠀⠀⠀⠀⣠⠒⠩⡀⠁⢀⡘⠀⠀⠀⣀⣼⢻⢺⠀⢸⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡸⡥⠓⠜⡏⣼⢱⣇⠣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠦⣄⠈⠀⢠⠁⡘⠀⠰⢹⡉⠛⢄⡀⣼⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢇⠋⡄⠱⡘⢰⣻⢮⡿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠲⣠⠊⢀⣰⠡⢃⠠⠙⡇⠠⣙⠒⣀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⠎⠀⢡⢀⢡⣜⠟⠸⣷⣣⢤⣀⡀⡀⣀⡀⠀⠀⠀⠀⠀⢀⡴⠶⣋⣤⡞⣯⠆⣘⡤⠲⠃⣔⣋⡲⠝⠒
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠋⠀⠀⠀⡎⢒⢫⠀⢽⠙⠚⠧⢯⣽⡱⣷⣤⡐⠠⠄⠤⠄⣤⣞⣿⠛⣽⠉⣰⡶⢤⠀⠡⢠⡐⢷⡶⠚⠛
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡲⠉⡰⠀⠀⠀⠸⢩⡄⣷⣸⠀⡡⠂⡄⢠⠀⡉⠉⢇⠐⠨⢐⠠⠸⢀⠠⢉⠉⠜⢣⠆⣌⠫⠅⡦⡇⢠⡇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢠⠝⠀⡰⠁⠀⡀⠀⠀⣟⡇⢸⣿⠠⢁⠒⡈⠄⣢⡴⣥⡎⢀⠃⠔⢂⢱⣀⢒⠠⢈⢐⣼⡓⢽⢻⣃⠓⡇⠸⢂⠀⠀
+⠀⠀⠀⠀⠀⠀⣰⠏⢀⠞⠁⠀⠀⠃⠀⠀⢸⢥⠘⢻⡔⠈⠰⠐⡘⠛⡉⢁⠇⠌⠒⡈⢤⣞⡼⠃⡐⢠⢞⣬⣣⡻⢉⠤⣿⡆⠀⣼⠀⠀
+⠀⠀⠀⠀⠀⡼⢂⠔⠁⠀⠀⠀⠀⠀⠀⠀⢸⠀⡏⣨⣷⠬⣤⠥⠄⢳⣘⣽⠶⢼⢢⣶⡋⠀⠄⢂⡴⢳⢦⣹⣦⠁⡋⠔⣏⢃⠀⠇⠆⠀
+⠀⠀⠀⠀⣨⠇⠁⠀⠀⠀⠀⠀⡄⠃⠀⠀⠌⠀⡇⠙⠛⠁⠀⠁⠀⠀⠀⠀⠉⠂⣕⢮⣭⠷⠸⣘⣎⢎⠯⣵⠋⢠⢋⠰⡯⠼⠀⢸⠃⠀
+⠀⠀⠀⡠⡇⠀⠀⠀⠀⠀⠀⢠⢀⠀⠀⡘⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⣿⠄⠄⠀⠀⠘⠿⠃⣸⠃⠀⡼⡄⢣⣟⡇⡀⠀⠀⠀
+⠀⠀⢰⠎⠀⠀⠀⠀⠀⠀⠀⠈⠸⠀⠰⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⢀⠞⠁⠀⢠⣿⣇⢢⠹⠄⡇⠀⠀⠀
+⠀⡘⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⡿⡇⠀⠀⠀⢾⡗⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠀⠀⢀⠀⠀⠁⠀⠀⠀⢦⣟⣧⢸⢽⣻⠇⠸⡆⠀
+⠮⠀⠀⠂⠀⠀⠀⠂⠠⠀⠀⢀⠃⡐⠇⠣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢿⠛⠤⣀⠉⠀⠀⠀⠀⠀⠀⠘⣾⠘⢧⡏⢶⡟⠇⣳⠁⠀
+        ]],
+
+        -- Footer to be displayed after items. Converted to single string via
+        -- `tostring` (use `\n` to display several lines). If function, it is
+        -- evaluated first. If `nil` (default), default usage help will be shown.
+        footer = nil,
+
+        -- Array  of functions to be applied consecutively to initial content.
+        -- Each function should take and return content for 'Starter' buffer (see
+        -- |mini.starter| and |MiniStarter.content| for more details).
+        content_hooks = nil,
+
+        -- Characters to update query. Each character will have special buffer
+        -- mapping overriding your global ones. Be careful to not add `:` as it
+        -- allows you to go into command mode.
+        query_updaters = 'abcdefghijklmnopqrstuvwxyz0123456789_-.',
+
+        -- Whether to disable showing non-error feedback
+        silent = false,
+      }
+
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
