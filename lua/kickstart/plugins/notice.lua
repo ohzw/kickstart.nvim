6fe452d0a588b3062df1d1160e73df1d5ced7745
ohzw <52651644+ohzw@users.noreply.github.com>
2024-05-23 19:19:33 +0900

    update notice config

--- lua/kickstart/plugins/notice.lua
+++ lua/kickstart/plugins/notice.lua
@@ -50,8 +50,8 @@ return {
           -- This is a current Neovim limitation.
           enabled = true, -- enables the Noice messages UI
           view = 'mini', -- default view for messages
-          view_error = 'notify', -- view for errors
-          view_warn = 'notify', -- view for warnings
+          view_error = 'mini', -- view for errors
+          view_warn = 'mini', -- view for warnings
           view_history = 'messages', -- view for :messages
           view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
         },
@@ -118,7 +118,7 @@ return {
           -- The default routes will forward notifications to nvim-notify
           -- Benefit of using Noice for this is the routing and consistent history view
           enabled = true,
-          view = 'notify',
+          view = 'mini',
           background_colour = '#000000',
         },
         lsp = {
