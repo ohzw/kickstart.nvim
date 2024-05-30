348cfd7f8c76e31b74d728d38dc284bc42ba36e2
ohzw <52651644+ohzw@users.noreply.github.com>
2024-05-30 19:05:43 +0900

    update bufferline config

--- lua/kickstart/plugins/bufferline.lua
+++ lua/kickstart/plugins/bufferline.lua
@@ -6,15 +6,20 @@ return {
     config = function()
       require('bufferline').setup {
         options = {
-          indicator = {
-            icon = '▎', -- this should be omitted if indicator style is not 'icon'
-            style = 'icon',
-          },
-          buffer_close_icon = '󰅖',
+          --   indicator = {
+          --     icon = '▎', -- this should be omitted if indicator style is not 'icon'
+          --     style = 'icon',
+          --   },
+          buffer_close_icon = '',
           modified_icon = '●',
           close_icon = '',
           left_trunc_marker = '',
           right_trunc_marker = '',
+          --   hover = {
+          --     enabled = true,
+          --     delay = 150,
+          --     reveal = { 'close' },
+          --   },
           offsets = {
             {
               filetype = 'neo-tree',
@@ -22,11 +27,6 @@ return {
               text_align = 'center',
               separator = true,
             },
-          },
-          hover = {
-            enabled = true,
-            delay = 150,
-            reveal = { 'close' },
           },
           diagnostics_indicator = function(count, level, diagnostics_dict, context)
             local s = ' '
