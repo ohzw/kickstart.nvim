9c052bbb829623c2356f762c48efeab34a6a026f
ohzw <52651644+ohzw@users.noreply.github.com>
2024-05-30 11:50:15 +0900

    configure themes

--- lua/kickstart/plugins/themes/cappuccin.lua
+++ lua/kickstart/plugins/themes/cappuccin.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require('catppuccin').setup {
-      flavour = 'auto', -- latte, frappe, macchiato, mocha
+      flavour = 'auto', -- auto, latte, frappe, macchiato, mocha
       background = { -- :h background
         light = 'latte',
         dark = 'mocha',
