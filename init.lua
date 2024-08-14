9fa9e92854b3740673e9086f9198921ba7634897
ohzw <52651644+ohzw@users.noreply.github.com>
2024-08-14 15:46:56 +0900

    add copilot

--- init.lua
+++ init.lua
@@ -1124,6 +1124,7 @@ require('lazy').setup({
   require 'kickstart.plugins.bufferline',
   require 'kickstart.plugins.neoclip',
   require 'kickstart.plugins.outline',
+  require 'kickstart.plugins.copilot',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
